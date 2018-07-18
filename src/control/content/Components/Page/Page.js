6f88652ab48fd062393089726d719dda13ec5687
lyonx <chrisb000@gmail.com>
2018-07-17 19:58:15 -0700

    foo

--- src/control/content/Components/Page/Page.js
+++ src/control/content/Components/Page/Page.js
@@ -2,247 +2,169 @@ import React, { Component } from "react";
 let db = buildfire.datastore;
 
 class Page extends Component {
-  constructor(props) {
-    super(props);
-    this.updatePage = props.updatePage;
-    this.deletePage = props.deletePage;
-    this.key = props.key;
-    // this.modal = document.createElement("div");
-    this.update = this.update.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.delete = this.delete.bind(this);
-    this.state = {
-      title: props.title,
-      plugins: props.plugins,
-      header: props.header,
-      desc: props.desc
-    };
-  }
+   constructor(props) {
+      super(props);
+      this.updatePage = props.updatePage;
+      this.deletePage = props.deletePage;
+      this.key = props.key;
+      // this.modal = document.createElement("div");
+      this.update = this.update.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+      this.renderImages = this.renderImages.bind(this);
+      this.delete = this.delete.bind(this);
+      this.addImg = this.addImg.bind(this);
+      this.state = {
+         title: props.data.title,
+         plugins: props.data.plugins,
+         header: props.data.header,
+         desc: props.data.desc,
+         images: props.data.images
+      };
+   }
 
-  initSortable() {
-    let selector = `#plugins${this.key}`;
-    let plugins = new buildfire.components.pluginInstance.sortableList(
-      selector,
-      [],
-      { showIcon: true, confirmDeleteItem: false },
-      undefined,
-      undefined,
-      { itemEditable: true, navigationCallback: () => console.log("nav cb") }
-    );
+   initSortable() {
+      let selector = `#plugins${this.key}`;
+      let plugins = new buildfire.components.pluginInstance.sortableList(selector, [], { showIcon: true, confirmDeleteItem: false }, undefined, undefined, { itemEditable: true, navigationCallback: () => console.log("nav cb") });
+      plugins.loadItems(this.state.plugins);
+      plugins.onAddItems = () => {
+         this.setState({
+            plugins: plugins.items
+         });
+         this.update();
+      };
+      plugins.onDeleteItem = () => {
+         this.setState({
+            plugins: plugins.items
+         });
+         this.update();
+      };
+      plugins.onOrderChange = () => {
+         this.setState({
+            plugins: plugins.items
+         });
+         this.update();
+      };
+   }
 
-    plugins.loadItems(this.state.plugins);
-    // db.get("plugins", (err, response) => {
-    //   if (err) throw err;
-    //   console.log(response);
-    //   plugins.loadItems(response.data.plugins);
-    //   if (!response.id) {
-    //     db.insert(
-    //       { plugins: plugins.items },
-    //       "plugins",
-    //       true,
-    //       (err, status) => {
-    //         if (err) {
-    //           console.log("insert err");
-    //           throw err;
-    //         }
-    //         console.log(status);
-    //       }
-    //     );
-    //   }
-    // });
+   delete() {
+      this.deletePage(this.key);
+   }
 
-    // plugins.loadItems(plugins.items, null);
-
-    plugins.onAddItems = () => {
-      this.setState({
-        plugins: plugins.items
+   update() {
+      console.log(this);
+      this.updatePage(this.key, {
+         title: this.state.title,
+         plugins: this.state.plugins,
+         header: this.state.header,
+         desc: this.state.desc,
+         images: this.state.images
       });
-    };
+   }
 
-    //   db.get("plugins", (err, response) => {
-    //     if (err) throw err;
-    //     console.log(response);
-    //     if (!response.id) {
-    //       db.insert(
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         true,
-    //         (err, status) => {
-    //           if (err) {
-    //             console.log("insert err");
-    //             throw err;
-    //           }
-    //           console.log(status);
-    //         }
-    //       );
-    //     } else {
-    //       db.update(
-    //         response.id,
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         (err, status) => {
-    //           if (err) throw err;
-    //           console.log(status);
-    //         }
-    //       );
-    //     }
-    //   });
-    // };
+   addImg() {
+      buildfire.imageLib.showDialog({}, (err, result) => {
+         if (err) throw err;
+         let images = this.state.images;
+         result.selectedFiles.forEach(image => {
+            images.push(image);
+         });
+         this.setState({ images });
+         this.update();
+      });
+   }
 
-    // plugins.onDeleteItem = () => {
-    //   db.get("plugins", (err, response) => {
-    //     if (err) throw err;
-    //     console.log(response);
-    //     if (!response.id) {
-    //       db.insert(
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         true,
-    //         (err, status) => {
-    //           if (err) {
-    //             console.log("insert err");
-    //             throw err;
-    //           }
-    //           console.log(status);
-    //         }
-    //       );
-    //     } else {
-    //       db.update(
-    //         response.id,
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         (err, status) => {
-    //           if (err) throw err;
-    //           console.log(status);
-    //         }
-    //       );
-    //     }
-    //   });
-    // };
-
-    // plugins.onOrderChange = () => {
-    //   db.get("plugins", (err, response) => {
-    //     if (err) throw err;
-    //     console.log(response);
-    //     if (!response.id) {
-    //       db.insert(
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         true,
-    //         (err, status) => {
-    //           if (err) {
-    //             console.log("insert err");
-    //             throw err;
-    //           }
-    //           console.log(status);
-    //         }
-    //       );
-    //     } else {
-    //       db.update(
-    //         response.id,
-    //         { plugins: plugins.items },
-    //         "plugins",
-    //         (err, status) => {
-    //           if (err) throw err;
-    //           console.log(status);
-    //         }
-    //       );
-    //     }
-    //   });
-    // };
-  }
-
-  delete() {
-    this.deletePage(this.key);
-  }
-
-  update() {
-    console.log(this);
-    this.updatePage(this.key, {
-      title: this.state.title,
-      plugins: this.state.plugins,
-      header: this.state.header,
-      desc: this.state.desc
-    });
-  }
-
-  edit() {}
-
-  handleChange(event) {
-    console.log("handlechange");
-    const target = event.target;
-    const name = target.name;
-    this.setState({ [name]: event.target.value });
-  }
-
-  componentDidMount() {
-    this.initSortable();
-  }
-
-  componentDidUpdate() {
-    console.log(this.state);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h3 className="panel-title">{this.props.title}</h3>
-          </div>
-          <div className="panel-body">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12">
-                <form>
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="title"
-                      aria-describedby="sizing-addon2"
-                      value={this.state.title}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="header"
-                      aria-describedby="sizing-addon2"
-                      value={this.state.header}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                  <div className="input-group">
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="desc"
-                      aria-describedby="sizing-addon2"
-                      value={this.state.desc}
-                      onChange={this.handleChange}
-                    />
-                  </div>
-                </form>
-                </div>
-                <div className="col-sm-12">
-                  <div id={`plugins${this.key}`} />
-                  <button className="btn btn-primary" onClick={this.delete}>
-                    Delete Page
-                  </button>
-                  <button className="btn btn-primary" onClick={this.update}>
-                    Update
-                  </button>
-                </div>
-              </div>
+   renderImages() {
+      let images = [];
+      console.log(this.props.data.images);
+      
+      this.props.data.images.forEach(image => {
+         images.push(
+            <div className="image-wrap">
+               <div className="images" style={`background: ${image}`} />
             </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+         );
+         return images;
+      });
+   }
+
+   handleChange(event) {
+      console.log("handlechange");
+      const target = event.target;
+      const name = target.name;
+      this.setState({ [name]: event.target.value });
+   }
+
+   componentDidMount() {
+      this.initSortable();
+   }
+
+   componentDidUpdate() {
+      console.log(this.state);
+   }
+
+   render() {
+      return (
+         <div>
+            <div className="panel panel-default">
+               <div className="panel-heading">
+                  <h3 className="panel-title">{this.props.title}</h3>
+               </div>
+               <div className="panel-body">
+                  <div className="container">
+                     <div className="row">
+                        <div className="col-sm-12">
+                           <form>
+                              <div className="input-group">
+                                 <input
+                                    type="text"
+                                    className="form-control"
+                                    name="title"
+                                    aria-describedby="sizing-addon2"
+                                    value={this.state.title}
+                                    onChange={this.handleChange}
+                                 />
+                              </div>
+                              <div className="input-group">
+                                 <input
+                                    type="text"
+                                    className="form-control"
+                                    name="header"
+                                    aria-describedby="sizing-addon2"
+                                    value={this.state.header}
+                                    onChange={this.handleChange}
+                                 />
+                              </div>
+                              <div className="input-group">
+                                 <input
+                                    type="text"
+                                    className="form-control"
+                                    name="desc"
+                                    aria-describedby="sizing-addon2"
+                                    value={this.state.desc}
+                                    onChange={this.handleChange}
+                                 />
+                              </div>
+                           </form>
+                        </div>
+                        <div className="col-sm-12">
+                           <div id={`plugins${this.key}`} />
+                           <button className="btn btn-primary" onClick={this.delete}>
+                              Delete Page
+                           </button>
+                           <button className="btn btn-primary" onClick={this.update}>
+                              Update
+                           </button>
+                           <button className="btn btn-primary" onClick={this.addImg}>
+                              Add Image
+                           </button>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default Page;
--- src/control/content/content.js
+++ src/control/content/content.js
@@ -6,154 +6,155 @@ let tinymce = window.tinymce;
 let db = buildfire.datastore;
 
 class Content extends Component {
-  constructor(props) {
-    super(props);
-    this.addPage = this.addPage.bind(this);
-    this.deletePage = this.deletePage.bind(this);
-    this.updatePage = this.updatePage.bind(this);
-    this.renderPages = this.renderPages.bind(this);
-    this.state = {
-      pages: [],
-      text: "",
-      img: ""
-    };
-  }
+   constructor(props) {
+      super(props);
+      this.addPage = this.addPage.bind(this);
+      this.deletePage = this.deletePage.bind(this);
+      this.updatePage = this.updatePage.bind(this);
+      this.renderPages = this.renderPages.bind(this);
+      this.state = {
+         pages: [],
+         text: "",
+         img: ""
+      };
+   }
 
-  fetch() {
-    // Control looks in db for any pages
-    db.get("pages", (err, response) => {
-      if (err) throw err;
-      console.log(response);
-      // if none are present, insert a default page
-      if (!response.id) {
-        this.setState({
-          pages: [
-            {
-              title: "new page",
-              header: "new page",
-              desc: "edit this page in the control",
-              plugins: []
+   fetch() {
+      // Control looks in db for any pages
+      db.get("pages", (err, response) => {
+         if (err) throw err;
+         console.log(response);
+         // if none are present, insert a default page
+         if (!response.id) {
+            this.setState({
+               pages: [
+                  {
+                     title: "new page",
+                     header: "new page",
+                     desc: "edit this page in the control",
+                     plugins: [],
+                     images: []
+                  }
+               ]
+            });
+         } else {
+            if (response.data.pages.length === 0) {
+               this.setState({
+                  pages: [
+                     {
+                        title: "New Page",
+                        header: "Example Header",
+                        desc: "Edit this page in the control",
+                        plugins: [],
+                        images: []
+                     }
+                  ]
+               });
+            } else {
+               this.setState({ pages: response.data.pages });
             }
-          ]
-        });
-      } else {
-        if (response.data.pages.length === 0) {
-          this.setState({
-            pages: [
-              {
-                title: "New Page",
-                header: "Example Header",
-                desc: "Edit this page in the control",
-                plugins: []
-              }
-            ]
-          });
-        } else {
-          this.setState({ pages: response.data.pages });
-        }
-      }
-    });
-  }
+         }
+      });
+   }
 
-  syncState() {
-    // when a state change is detected,
-    console.log(this.state);
-    db.get("pages", (err, response) => {
-      if (err) throw err;
-      console.log(response);
-      if (!response.id) {
-        return;
-      } else {
-        // insert pages into db
-        db.update(
-          response.id,
-          { pages: this.state.pages },
-          "pages",
-          (err, status) => {
-            if (err) throw err;
-            console.log(status);
-          }
-        );
-      }
-    });
-  }
+   syncState() {
+      // when a state change is detected,
+      console.log(this.state);
+      db.get("pages", (err, response) => {
+         if (err) throw err;
+         console.log(response);
+         if (!response.id) {
+            db.insert({ pages: this.state.pages }, "pages", true, (err, status) => {
+               if (err) throw err;
+               console.log(status);
+            });
+            return;
+         } else {
+            // insert pages into db
+            db.update(
+               response.id,
+               { pages: this.state.pages },
+               "pages",
+               (err, status) => {
+                  if (err) {
+                     throw err;
+                  }
+                  console.log(status);
+               }
+            );
+         }
+      });
+   }
 
-  renderPages() {
-    let pages = [];
-    this.state.pages.map(page => {
-      pages.push(
-        <Page
-          key={this.state.pages.indexOf(page)}
-          deletePage={this.deletePage}
-          updatePage={this.updatePage}
-          title={page.title}
-          header={page.header}
-          desc={page.desc}
-          plugins={page.plugins}
-        />
-      );
-    });
-    return pages;
-  }
+   renderPages() {
+      let pages = [];
+      this.state.pages.map(page => {
+         pages.push(
+            <Page key={this.state.pages.indexOf(page)} updatePage={this.updatePage} data={page}/>
+         );
+      });
+      return pages;
+   }
 
-  addPage() {
-    let newPage = {
-      title: "New Page",
-      header: "Example Header",
-      desc: "Edit this page in the control",
-      plugins: []
-    };
-    let pages = this.state.pages;
-    pages.push(newPage);
-    this.setState({ pages: pages });
-  }
+   addPage() {
+      let newPage = {
+         title: "New Page",
+         header: "Example Header",
+         desc: "Edit this page in the control",
+         plugins: [],
+         images: []
+      };
+      let pages = this.state.pages;
+      pages.push(newPage);
+      this.setState({ pages: pages });
+   }
 
-  deletePage(index) {
-    let pages = this.state.pages;
-    pages.splice(index, 1);
-    this.setState({ pages: pages });
-  }
+   deletePage(index) {
+      let pages = this.state.pages;
+      pages.splice(index, 1);
+      this.setState({ pages: pages });
+   }
 
-  updatePage(index, page) {
-    // console.log(index, page);
-    let pages = this.state.pages;
-    // console.log(pages);
-    pages[index] = page;
-    this.setState({ pages: pages });
-  }
+   updatePage(index, page) {
+      // console.log(index, page);
+      let pages = this.state.pages;
+      // console.log(pages);
+      pages[index] = page;
+      this.setState({ pages: pages });
+   }
 
-  componentDidMount() {
-    // Control looks in db for any pages
-    this.fetch();
-    // this.initSortable();
-    // this.mceInit();
-  }
+   componentDidMount() {
+      // Control looks in db for any pages
+      this.fetch();
+      // this.initSortable();
+      // this.mceInit();
+   }
 
-  componentDidUpdate() {
-    this.syncState();
-  }
+   componentDidUpdate() {
+      this.syncState();
+   }
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">Add Page</h3>
-              </div>
-              <div className="panel-body">
-                <button className="btn btn-primary" onClick={this.addPage}>
-                  Add a Page
+   render() {
+      return (
+         <div className="container-fluid">
+            <div className="row">
+               <div className="col-md-12">
+                  <div className="panel panel-default">
+                     <div className="panel-heading">
+                        <h3 className="panel-title">Add Page</h3>
+                     </div>
+                     <div className="panel-body">
+                        <button className="btn btn-primary" onClick={this.addPage}>
+                           Add a Page
                 </button>
-              </div>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-12">{this.renderPages()}</div>
             </div>
-          </div>
-          <div className="col-md-12">{this.renderPages()}</div>
-        </div>
-      </div>
-    );
-  }
+         </div>
+      );
+   }
 }
 
 export default Content;
